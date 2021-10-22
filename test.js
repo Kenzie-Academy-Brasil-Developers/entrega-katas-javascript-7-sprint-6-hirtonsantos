f54344fe018f87efa44599ad8f02238f3d34b8bd
hirtonsantos <hirtonsantos.hs@gmail.com>
2021-10-22 14:50:17 -0300

    corrgindo

--- test.js
+++ test.js
@@ -1,23 +1,20 @@
+const array1 = [1, 2, 3, 4, 5];
+let resul = 0
 
+function reduce(acumulador, valorAtual, index, array) {
+    resul = acumulador + valorAtual
+    if (index === array.length-1){
+        console.log(acumulador)
+    }
+}
 
-// const array1 = [1, 2, 3, 4, 5];
-// let resul = 0
+function newReduce(array, callback,valorinicial) {
+    if (valorinicial !== undefined){
+        resul+=valorinicial
+    }
+    for (let i = 0; i < array.length; i++) {
+        callback(resul, array[i], i, array)
+    }
+}
 
-// function reduce(acumulador, valorAtual, index, array) {
-//     resul = acumulador + valorAtual
-//     if (index === array.length-1){
-//         console.log(acumulador)
-//     }
-// }
-
-// function newReduce(array, callback,valorinicial) {
-//     if (valorinicial !== undefined){
-//         resul+=valorinicial
-//     }
-//     for (let i = 0; i < array.length; i++) {
-//         callback(resul, array[i], i, array)
-//     }
-// }
-
-// let retornonewReduce = newReduce(array1, reduce, 5)
-
+let retornonewReduce = newReduce(array1, reduce, 5)
